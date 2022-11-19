9a0aeae5d5f10f108e4c1a76b1b47e641b211e01
x4trm <damian.maza@wp.pl>
2022-11-19 17:39:23 +0100

    update all and add about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,44 @@
 <template>
-  
+  <div class="content">
+    <div v-for="i in info" :key="i.name" class="card">
+        <p class="name">{{i.name}}</p>
+        <p class="date">{{i.date}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            info:[
+                {name:"Utworzenie Systemu Logowania i Rejestracji",date:"Grudzień 2022"},
+                {name:"Utworzenie Modeli Bazodanowych",date:"Listopad 2022"},
+                {name:"Utworzenie Strony Internetowej",date:"Listopad 2022"},
+                {name:"Badanie Potrzeb Rynkowych",date:"Listopad 2022"},
+                {name:"Pomysł Projektu",date:"Październik 2022"}
+            ],
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.card{
+    border-radius:40px;
+    background-color: white;
+    width:300px;
+    height:200px;
+} 
+.name{
+    color: #41b883;
+    padding:10px;
+    font-size:21px;
+    font-weight: 700;
+}
+.date{
+    
+    font-size:18px;
+    color:#2c3e50;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,37 @@
 <template>
   <h1>Sentakki</h1>
   <div class="main">
+    <img :src="require(`@/assets/${logo}`)" alt="logo">
     <div class="content">
         <p>Sentakki w języku japońskim oznacza pralkę. Zespół programistów z <a href="https://www.fizyka.umk.pl/" target="_blank">Wydziału Fizyki, Astronomii i Informatyki Stosowanej</a> podjął się stworzenia 
             systemu zarządzania pralniami w akademikach
- <a href="https://www.umk.pl/" target="_blank">Uniwersytetu Mikołaja Kopernika</a> w Tourniu. Projekt jest realizowany w ramach przedmiotu Programowanie Zespołowe.</p>
+ <a href="https://www.umk.pl/" target="_blank">Uniwersytetu Mikołaja Kopernika</a> w Toruniu. Projekt jest realizowany w ramach przedmiotu Programowanie Zespołowe.</p>
     </div>
 </div>
   </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            logo:"logo.png",
+        }
+    }
 }
 </script>
 
 <style>
+img{
+    width:350px;
+
+}
 .main{
-    max-width: 50rem;
+    max-width: 50em;
 	margin: 0 auto;
 }
 .content{
-	margin: 0 auto;
+	margin: 6px;
+    font-size:1rem;
 }
 a{
     text-decoration: none;
@@ -32,7 +42,7 @@ h1{
     font-size:3rem;
 }
 p{
-    margin-top:20px;
-    font-size: 2rem;
+    margin-top:5px;
+    font-size: 1.75em;
 }
 </style>
