6ac1768558a399142ee83219df076edf3be57c56
x4trm <damian.maza@wp.pl>
2022-12-24 17:16:38 +0100

    update stack

--- src/views/TechnologyView.vue
+++ src/views/TechnologyView.vue
@@ -1,5 +1,28 @@
 <template>
-  <h1>Web</h1>  
+    <h1>Backend</h1>  
+    <div class="conteiner">
+        <div class="element">
+            <img :src="require(`@/assets/${asp}`)" alt="asp-net-logo">
+            <p>ASP.net</p>
+        </div>
+        <div class="element">
+            <img :src="require(`@/assets/${jwt}`)" alt="jwt-logo">
+            <p>JSON Web Token</p>
+        </div>             
+    </div>
+    <h1>Database</h1>  
+    <div class="conteiner">
+        <div class="element">
+            <img :src="require(`@/assets/${postgresql}`)" alt="postgresql-logo">
+        </div>
+    </div>
+    <h1>Frontend</h1>  
+    <div class="conteiner">
+        <div class="element">
+            <img :src="require(`@/assets/${react}`)" alt="react-native-logo">
+        </div>
+    </div>
+    <h1>Web</h1>  
     <div class="conteiner">
         <div class="element">
             <img :src="require(`@/assets/${vue}`)" alt="vue-logo">
@@ -9,9 +32,13 @@
     <h1>Other</h1>
     <div class="conteiner">
         <div class="element">
+            <img :src="require(`@/assets/${docker}`)" alt="docker-logo">
+            <p>Docker</p>
+        </div>
+        <div class="element">
             <img :src="require(`@/assets/${github}`)" alt="github-logo">
             <p>Github</p>
-        </div>
+        </div>                
     </div>
 </template>
 
@@ -20,7 +47,12 @@ export default {
     data(){
         return{
             vue:"vue-logo.png",
-            github:"github-logo.png"
+            github:"github-logo.png",
+            asp:"asp-net-logo.png",
+            jwt:"jwt-logo.png",
+            postgresql:"postgresql-logo.png",
+            docker:"docker-logo.png",
+            react:"react-native-logo.png"
         }
     }
 }
@@ -41,5 +73,9 @@ p{
 .element{
     padding: 5px;
     margin:15px;
+}
+img{
+    width: 220px;
+    height: 200px;
 }
 </style>
