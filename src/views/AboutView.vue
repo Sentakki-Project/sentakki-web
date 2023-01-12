deb00fe649d7f901bd9320351b77426014e0b22a
x4trm <damian.maza@wp.pl>
2023-01-12 02:33:18 +0100

    update about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,54 +1,49 @@
 <template>
-  <div class="content">
-    <div v-for="i in info" :key="i.name" class="card">
-        <p class="name">{{i.name}}</p>
-        <p class="date">{{i.date}}</p>
-    </div>
+  <h1>Panel Administracyjny</h1>
+  <div v-for="item in adminItems" :key="item.text">
+    <li>{{ item.text }}</li>
+  </div>
+  <h1>Panel Użytkownika</h1>
+  <div v-for="item in userItems" :key="item.text">
+    <li>{{ item.text }}</li>
   </div>
 </template>
-
 <script>
-export default {
-    data(){
-        return{
-            info:[
-                {name:"Utworzenie Systemu Logowania i Rejestracji",date:"Grudzień 2022"},
-                {name:"Utworzenie Modeli Bazodanowych",date:"Listopad 2022"},
-                {name:"Utworzenie Strony Internetowej",date:"Listopad 2022"},
-                {name:"Badanie Potrzeb Rynkowych",date:"Listopad 2022"},
-                {name:"Pomysł Projektu",date:"Październik 2022"}
-            ],
-        }
-    }
-}
+  export default {
+    data () {
+      return {
+        tab: null,
+        adminItems: [
+          { text: 'Logowanie'},
+          { text: 'podgląd stanu rezerwacji pralek'},
+          { text: 'ustawienie godzin otwarcia pralni'},
+          { text: 'ustawienie liczby dostępnych pralek'},
+          { text: 'ustawienie po ilu zarezerwowanych, ale niewykorzystanych praniach użytkownik zostanie zablokowany i na ile czasu'},
+          { text: 'ustawienie ile czasu przed można odwołać pranie'},
+          { text: 'ustawienie maksymalnej liczby aktywnych rezerwacji na użytkownika'},
+          { text: 'edytowanie rezerwacji'},
+        ],
+        userItems:[
+        { text: 'logowanie kontem USOS'},
+        { text: 'wybór akademika'},
+        { text: 'podgląd stanu rezerwacji pralek'},
+        { text: 'rezerwacja slotu na pralkę'},
+        { text: 'podgląd rezerwacji użytkownika'},
+        { text: 'anulowanie rezerwacji'},
+        { text: 'powiadomienie poprzedniego użytkownika pralki by wyjął rzeczy'},
+        { text: 'otrzymanie powiadomienia o tym że skończyło się pranie'},
+        { text: 'płatność za pranie'},
+        ]
+      }
+    },
+  }
 </script>
-
 <style scoped>
-.content{
-    height:700px;
-    padding:20px;
-    margin:20px 10px;
-    display: flex;
-    justify-content: center;
-    flex-wrap:wrap;
+
+h1{
+  font-size:35px;
 }
-.card{
-    border-radius:40px;
-    background-color: white;
-    width:300px;
-    height:200px;
-    margin:20px;
-} 
-.name{
-    color: #41b883;
-    padding:10px;
-    font-size:21px;
-    font-weight: 700;
-    margin-top:1.5rem;
-}
-.date{
-    
-    font-size:18px;
-    color:#2c3e50;
+li{
+  font-size:20px;
 }
 </style>
